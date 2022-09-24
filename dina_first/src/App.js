b881a5b448da88e412168230f10a360ba963818c
Adam Bihacker <adam.bihacker@gmail.com>
2022-09-24 15:55:47 +0200

    basic form component with some styles added

---
+++ dina_first/src/App.js
@@ -0,0 +1,17 @@
+import Form from './components/Form'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <h1>Number Converter</h1>
+      </header>
+      <div className="container">
+        <Form/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ dina_first/src/components/Form.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import numberConverter from '../utils/ConverterFunction';
+import '../App.css'
+
+const Form = () => {
+  const [number, setNumber] = useState("");
+  const [numberStringName, setNumberStringName] = useState();
+  
+  return (
+      <form>
+        <h2>Convert your number</h2>
+        <input
+          type="number"
+          required
+          placeholder="Type a number"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+        />
+
+        <button onClick={(e) => setNumberStringName()}>Convert</button>
+        <h3>Output: {numberStringName} </h3>
+      </form>
+  );
+};
+
+export default Form;
