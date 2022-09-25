0e0a076bf9845516d27b860efa9a9205faeb05c7
Adam Bihacker <adam.bihacker@gmail.com>
2022-09-25 10:13:57 +0200

    logical function done

--- dina_first/src/components/Form.jsx
+++ dina_first/src/components/Form.jsx
@@ -17,8 +17,8 @@ const Form = () => {
           onChange={(e) => setNumber(e.target.value)}
         />
 
-        <button onClick={(e) => setNumberStringName()}>Convert</button>
-        <h3>Output: {numberStringName} </h3>
+        <button onClick={(e) => setNumberStringName(numberConverter(e, number))}>Convert</button>
+        <h3> {numberStringName} </h3>
       </form>
   );
 };
--- dina_first/src/utils/ConverterFunction.js
+++ dina_first/src/utils/ConverterFunction.js
@@ -1,1 +1,92 @@
-// CONVERTER FUNCTION GOES HERE
+const NumberConverter = (e, number) => {
+  e.preventDefault(); 
+
+ let outputText;
+ let numberInput = number;
+
+ let digitsToNineteenArr = [
+   "",
+   "one ",
+   "two ",
+   "three ",
+   "four ",
+   "five ",
+   "six ",
+   "seven ",
+   "eight ",
+   "nine ",
+   "ten ",
+   "eleven ",
+   "twelve ",
+   "thirteen ",
+   "fourteen ",
+   "fifteen ",
+   "sixteen ",
+   "seventeen ",
+   "eighteen ",
+   "nineteen ",
+ ];
+ const decimals = [
+   "",
+   "",
+   "twenty",
+   "thirty",
+   "forty",
+   "fifty",
+   "sixty",
+   "seventy",
+   "eighty",
+   "ninety",
+ ];
+
+ if (numberInput.toString().length > 7) return alert("Too big number"); //Checking the incoming numbers length
+ console.log(numberInput);
+
+ let numberArr = ("0000000" + numberInput)
+   .slice(-7)
+   .match(/^(\d{1})(\d{1})(\d{2})(\d{1})(\d{2})$/); //Array for the numbers, with slice and and a regex, to separate the 
+ console.log(numberArr);
+ if (!numberArr) return;
+ //console.log(numberArr[0], numberArr[1], numberArr[2], numberArr[3], numberArr[4], numberArr[5])
+
+ //Conditional rendering part
+ outputText =
+   numberArr[1] != 0
+     ? (digitsToNineteenArr[Number(numberArr[1])] ||
+         decimals[numberArr[1][0]] +
+           " " +
+           digitsToNineteenArr[numberArr[1][1]]) + " million "
+     : "";
+
+ outputText +=
+   numberArr[2] != 0
+     ? (digitsToNineteenArr[Number(numberArr[2])] ||
+         decimals[numberArr[2][0]] +
+           " " +
+           digitsToNineteenArr[numberArr[2][1]]) + " hundred "
+     : "";
+ outputText +=
+   numberArr[3] != 0
+     ? (digitsToNineteenArr[Number(numberArr[3])] ||
+         decimals[numberArr[3][0]] +
+           " " +
+           digitsToNineteenArr[numberArr[3][1]]) + " thousand "
+     : "";
+ outputText +=
+   numberArr[4] != 0
+     ? (digitsToNineteenArr[Number(numberArr[4])] ||
+         decimals[numberArr[4][0]] +
+           " " +
+           digitsToNineteenArr[numberArr[4][1]]) + " hundred and "
+     : "";
+ outputText +=
+   numberArr[5] != 0
+     ? digitsToNineteenArr[Number(numberArr[5])] ||
+       decimals[numberArr[5][0]] +
+         " " +
+         digitsToNineteenArr[numberArr[5][1]]
+     : "";
+     return outputText;    
+};
+
+export default NumberConverter;
