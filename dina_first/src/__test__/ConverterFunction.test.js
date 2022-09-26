ed4fdc17e915cf9fd00cb788fb70c47a872bbc13
Adam Bihacker <adam.bihacker@gmail.com>
2022-09-26 13:04:14 +0200

    unit tests added

--- dina_first/src/__test__/ConverterFunction.test.js
+++ dina_first/src/__test__/ConverterFunction.test.js
@@ -1,5 +1,9 @@
 import numberConverter from "../utils/ConverterFunction";
 
+test("convert 5 to five", () => {
+  expect(numberConverter(5)).toEqual("five ");
+});
+
 test("convert 12 to twelve", () => {
   expect(numberConverter(12)).toEqual("twelve ");
 });
@@ -8,12 +12,24 @@ test("convert 99 to ninety nine", () => {
   expect(numberConverter(99)).toEqual("ninety - nine ");
 });
 
+test("convert 645 to six hundred and forty - five", () => {
+  expect(numberConverter(645)).toEqual('six  hundred and forty - five ')
+})
+
 test("convert 3542 to three thousand five hundred and forty two", () => {
   expect(numberConverter(3542)).toEqual('three  thousand five  hundred and forty - two ');
 })
 
+test("convert 42342 to forty two thousand three hundred and forty - two", () => {
+  expect(numberConverter(42342)).toEqual('forty  two  thousand three  hundred and forty - two ');
+})
+
+test("convert 322132 to three hundred and twenty one thousand one hundred and thirty-two ", () => {
+  expect(numberConverter(322132)).toEqual('three hundred and twenty  two  thousand one  hundred and thirty - two ');
+})
+
 test("convert 1000000 to one million", () => {
-  expect(numberConverter(1000000)).toEqual('one million');
+  expect(numberConverter(1000000)).toEqual('one million ');
 })
 
 test("convert 10.000.000", () => {
--- dina_first/src/utils/ConverterFunction.js
+++ dina_first/src/utils/ConverterFunction.js
@@ -27,18 +27,19 @@ const numberConverter = (number) => {
   const decimals = [
     "",
     "",
-    "twenty -",
-    "thirty -",
-    "forty -",
-    "fifty -",
-    "sixty -",
-    "seventy -",
-    "eighty -",
-    "ninety -",
+    "twenty ",
+    "thirty ",
+    "forty ",
+    "fifty ",
+    "sixty ",
+    "seventy ",
+    "eighty ",
+    "ninety ",
   ];
 
   if (numberInput.toString().length > 7) {
-    outputText += "Too big number";
+    outputText = "Too big number";
+    return outputText;
   }
 
   let numberArr = ("0000000" + numberInput)
@@ -47,22 +48,27 @@ const numberConverter = (number) => {
   console.log(numberArr);
   if (!numberArr) return;
   //console.log(numberArr[0], numberArr[1], numberArr[2], numberArr[3], numberArr[4], numberArr[5])
-
   outputText =
     numberArr[1] != 0
       ? (digitsToNineteenArr[Number(numberArr[1])] ||
           decimals[numberArr[1][0]] +
             " " +
-            digitsToNineteenArr[numberArr[1][1]]) + "million"
+            digitsToNineteenArr[numberArr[1][1]]) + "million "
       : "";
 
-  outputText +=
-    numberArr[2] != 0
-      ? (digitsToNineteenArr[Number(numberArr[2])] ||
-          decimals[numberArr[2][0]] +
-            " " +
-            digitsToNineteenArr[numberArr[2][1]]) + "hundred "
-      : "";
+  if (numberArr[2] != 0 && numberArr[3] % 10 == 0) {
+    outputText +=
+      digitsToNineteenArr[Number(numberArr[2])] + "hundred thousand ";
+  } else if (numberArr[2] != 0) {
+    outputText +=
+      (digitsToNineteenArr[Number(numberArr[2])] ||
+        decimals[numberArr[2][0]] +
+          " " +
+          digitsToNineteenArr[numberArr[2][1]]) + "hundred and ";
+  } else {
+    outputText += "";
+  }
+
   outputText +=
     numberArr[3] != 0
       ? (digitsToNineteenArr[Number(numberArr[3])] ||
@@ -70,26 +76,40 @@ const numberConverter = (number) => {
             " " +
             digitsToNineteenArr[numberArr[3][1]]) + " thousand "
       : "";
-  outputText +=
-    numberArr[4] != 0
-      ? (digitsToNineteenArr[Number(numberArr[4])] ||
-          decimals[numberArr[4][0]] +
-            " " +
-            digitsToNineteenArr[numberArr[4][1]]) + " hundred and "
-      : "";
 
-  outputText +=
-    numberArr[5] != 0
-      ? digitsToNineteenArr[Number(numberArr[5])] ||
-        decimals[numberArr[5][0]] + " " + digitsToNineteenArr[numberArr[5][1]]
-      : "";
+  if (numberArr[4] != 0 && numberArr[5] == 0) {
+    outputText +=
+      (digitsToNineteenArr[Number(numberArr[4])] ||
+        decimals[numberArr[4][0]] +
+          " " +
+          digitsToNineteenArr[numberArr[4][1]]) + " hundred";
+  } else if (numberArr[4] != 0) {
+    outputText +=
+      (digitsToNineteenArr[Number(numberArr[4])] ||
+        decimals[numberArr[4][0]] +
+          " " +
+          digitsToNineteenArr[numberArr[4][1]]) + " hundred and ";
+  } else {
+    outputText += "";
+  }
 
-  if (numberInput.toString().length > 7) {
-    outputText += "Too big number";
-  } else if (numberInput == 0) {
-    outputText += "zero";
-  } else if (numberInput < 0) {
-    outputText += "Only positive integers allowed";
+  if (numberArr[5] != 0 && numberArr[5] % 10 == 0) {
+    outputText +=
+      digitsToNineteenArr[Number(numberArr[5])] ||
+      decimals[numberArr[5][0]] + " " + digitsToNineteenArr[numberArr[5][1]];
+  } else if (numberArr[5] != 0) {
+    outputText +=
+      digitsToNineteenArr[Number(numberArr[5])] ||
+      decimals[numberArr[5][0]] + "- " + digitsToNineteenArr[numberArr[5][1]];
+  } else {
+    outputText += "";
+  }
+
+  if (numberInput == 0) {
+    outputText = "zero";
+  }
+  if (numberInput < 0) {
+    outputText = "Only positive integers allowed";
   }
 
   return outputText;
